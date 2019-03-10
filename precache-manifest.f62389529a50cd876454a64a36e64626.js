dc29479c815a7d438ad7da7fe8299f72d2c6fc18
MitevskaS <mitevska138@gmail.com>
2019-03-10 15:45:22 +0100

    Updates

---
+++ precache-manifest.f62389529a50cd876454a64a36e64626.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "639195f047898ea1316e",
+    "url": "/RandomQuoteMachine/static/css/main.362889f2.chunk.css"
+  },
+  {
+    "revision": "639195f047898ea1316e",
+    "url": "/RandomQuoteMachine/static/js/main.639195f0.chunk.js"
+  },
+  {
+    "revision": "f6acc09586ffbd706d99",
+    "url": "/RandomQuoteMachine/static/js/runtime~main.f6acc095.js"
+  },
+  {
+    "revision": "03b2c84d68d47afd8308",
+    "url": "/RandomQuoteMachine/static/css/2.b40aa33a.chunk.css"
+  },
+  {
+    "revision": "03b2c84d68d47afd8308",
+    "url": "/RandomQuoteMachine/static/js/2.03b2c84d.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/RandomQuoteMachine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fa4dd0a8fd91b5a6c1414020175bc47d",
+    "url": "/RandomQuoteMachine/index.html"
+  }
+];
